7ea2f0f7eaaec7ec1bf3a447585ac12b843459ce
bazaan <91964980+bazaan@users.noreply.github.com>
2023-09-02 22:42:21 -0500

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int numA = 15; // se puede cambiar
+        int numB = 8; // se puede cambiar
+        if (numA > 0 && numB > 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
